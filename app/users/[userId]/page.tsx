2aa827bee528475804c974d5c841a6d20323cc36
Muskulee <muskulee@gmail.com>
2024-02-14 14:36:17 +0100

    Added Dynamic Metadata

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,4 +1,24 @@
-const userDetails = ({ params }: { params: { userId: string } }) => {
+import { Metadata } from "next";
+
+type Props = {
+  params: { userId: string };
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`New User Ademimpe ${params.userId}`);
+    }, 1000);
+  });
+
+  return {
+    title: `${title}`,
+  };
+};
+
+const userDetails = ({ params }: Props) => {
   return <h1>User details goes in here. {params.userId}</h1>;
 };
 
