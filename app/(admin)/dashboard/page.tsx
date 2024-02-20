36eab993990979191f6db20c093c6b95118458b7
Muskulee <muskulee@gmail.com>
2024-02-20 05:23:43 +0100

    Added Transaction Table, And Data With Pagination

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -7,6 +7,7 @@ import {
   useCallback,
   Key,
   useState,
+  useMemo,
 } from "react";
 import { TiEyeOutline } from "react-icons/ti";
 import { MdDeleteOutline, MdOutlineShowChart } from "react-icons/md";
@@ -24,10 +25,17 @@ import {
   Tooltip,
   Chip,
   Button,
+  Pagination,
 } from "@nextui-org/react";
 
 import DashCard from "@/app/_lib/smalls/DashCard";
-import { BiEdit } from "react-icons/bi";
+import { BiEdit, BiMailSend } from "react-icons/bi";
+import {
+  testUsers,
+  testTransactions,
+  transactionColumns,
+  columns,
+} from "./../../_lib/api/fakeData";
 
 type DashBoardProps = {
   users: Array<{
@@ -40,74 +48,32 @@ type DashBoardProps = {
     avatar: string;
     email: string;
   }>;
+  transactions: Array;
   statusColorMap?: { [key: string]: string };
 };
 
-const Dashboard = ({ users }: DashBoardProps): ReactElement => {
+const Dashboard = ({ users, transactions }: DashBoardProps): ReactElement => {
   const [selectedKeys, setSelectedKeys]: [
     Set<number>,
     Dispatch<SetStateAction<Set<number>>>
   ] = useState(new Set());
   const [page, setPage] = useState(1);
+  const rowsPerPage = 4;
+
+  const pages = Math.ceil(testTransactions.length / rowsPerPage);
+
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return testTransactions.slice(start, end);
+  }, [page, testTransactions]);
 
   useEffect(() => {
-    // You can expose any functions or logic here
+    // Rewrite logic
 
     console.log("selectedKeys", selectedKeys);
   }, [selectedKeys]);
-
-  const testUsers = [
-    {
-      id: 1,
-      name: "Tony Reichert",
-      role: "CEO",
-      team: "Management",
-      status: "active",
-      age: "29",
-      avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-      email: "tony.reichert@example.com",
-    },
-    {
-      id: 2,
-      name: "Zoey Lang",
-      role: "Technical Lead",
-      team: "Development",
-      status: "paused",
-      age: "25",
-      avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-      email: "zoey.lang@example.com",
-    },
-    {
-      id: 3,
-      name: "Jane Fisher",
-      role: "Senior Developer",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 4,
-      name: "William Howard",
-      role: "Community Manager",
-      team: "Marketing",
-      status: "vacation",
-      age: "28",
-      avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
-      email: "william.howard@example.com",
-    },
-    {
-      id: 5,
-      name: "Kristen Copper",
-      role: "Sales Manager",
-      team: "Sales",
-      status: "active",
-      age: "24",
-      avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
-      email: "kristen.cooper@example.com",
-    },
-  ];
 
   const renderCell = useCallback(
     (
@@ -180,25 +146,82 @@ const Dashboard = ({ users }: DashBoardProps): ReactElement => {
     },
     []
   );
+  const renderTransaction = useCallback(
+    (
+      transaction: {
+        name?: string;
+        role?: string;
+        avatar?: string;
+        team?: string;
+        status?: string;
+        action?: string;
+        email?: string;
+      },
+      columnkey: Key
+    ) => {
+      const cellValue = transaction[columnkey];
 
-  const columns = [
-    {
-      key: "name",
-      label: "NAME",
+      const statusColorMap = {
+        active: "success",
+        paused: "danger",
+        vacation: "warning",
+      };
+      switch (columnkey) {
+        case "name":
+          return (
+            <User
+              avatarProps={{ radius: "lg", src: transaction.avatar }}
+              description={transaction.email}
+              name={cellValue}
+            >
+              {transaction.name}
+            </User>
+          );
+
+        case "action":
+          return (
+            <div className="relative flex items-center gap-2">
+              <Tooltip content="Details">
+                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                  <TiEyeOutline />
+                </span>
+              </Tooltip>
+              <Tooltip content="Edit transaction">
+                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                  <BiEdit />
+                </span>
+              </Tooltip>
+              <Tooltip color="primary" content="Send mail">
+                <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                  <BiMailSend />
+                </span>
+              </Tooltip>
+              <Tooltip color="danger" content="Delete transaction">
+                <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                  <MdDeleteOutline />
+                </span>
+              </Tooltip>
+            </div>
+          );
+
+        case "status":
+          return (
+            <Chip
+              className="capitalize"
+              color={statusColorMap[transaction?.status]}
+              size="sm"
+              variant="flat"
+            >
+              {cellValue}
+            </Chip>
+          );
+
+        default:
+          return cellValue;
+      }
     },
-    {
-      key: "role",
-      label: "ROLE",
-    },
-    {
-      key: "status",
-      label: "STATUS",
-    },
-    {
-      key: "action",
-      label: "ACTIONS",
-    },
-  ];
+    []
+  );
 
   return (
     <>
@@ -241,8 +264,9 @@ const Dashboard = ({ users }: DashBoardProps): ReactElement => {
           />
         </div>
 
-        <div className="lg:flex">
-          <div className="lg:w-2/3 lg:pr-4">
+        <div className="lg:flex lg:space-x-1">
+          {/* // Users Table */}
+          <div className="lg:w-2/3 ">
             <Table
               aria-label="Users Action Tables"
               selectionMode="multiple"
@@ -264,6 +288,10 @@ const Dashboard = ({ users }: DashBoardProps): ReactElement => {
                   </Button>
                 </div>
               }
+              // classNames={{
+              //   base: "max-h-[520px] overflow-scroll",
+              //   table: "min-h-[420px]",
+              // }}
             >
               <TableHeader columns={columns}>
                 {(column) => (
@@ -286,18 +314,61 @@ const Dashboard = ({ users }: DashBoardProps): ReactElement => {
               </TableBody>
             </Table>
           </div>
-
           {/* Second Column (Large Screen and above) */}
-          <div className="lg:w-1/3 lg:pl-4">
-            {/* Content for the second column */}
-            <p>Column 2 Content</p>
+          <div className="lg:w-1/3 ">
+            {/* Transactions Table for the second column */}
+            <Table
+              aria-label="Transactions Action Tables"
+              selectionMode="multiple"
+              className="px-8 lg:mt-5"
+              selectedKeys={selectedKeys}
+              onSelectionChange={setSelectedKeys}
+              isHeaderSticky
+              bottomContent={
+                <div className="flex w-full justify-center">
+                  <Pagination
+                    isCompact
+                    showControls
+                    showShadow
+                    color="secondary"
+                    page={page}
+                    total={pages}
+                    onChange={(page) => setPage(page)}
+                  />
+                </div>
+              }
+              classNames={{
+                wrapper: "min-h-[222px]",
+              }}
+            >
+              <TableHeader columns={transactionColumns}>
+                {(column) => (
+                  <TableColumn
+                    key={column.key}
+                    align={column.key === "action" ? "center" : "start"}
+                  >
+                    {column.label}
+                  </TableColumn>
+                )}
+              </TableHeader>
+              <TableBody items={items}>
+                {(item) => (
+                  <TableRow key={item.key}>
+                    {(columnKey: Key) => (
+                      <TableCell>
+                        {renderTransaction(item, columnKey)}
+                      </TableCell>
+                    )}
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
           </div>
-
-          {/* On Small Screens */}
-          <div className="sm:block lg:hidden">
-            {/* Content for small screens (stacked) */}
-            <p>Content for Small Screens</p>
-          </div>
+        </div>
+        {/* On Small Screens */}
+        <div className="sm:block lg:hidden">
+          {/* Content for small screens (stacked) */}
+          <p>Content for Small Screens</p>
         </div>
       </div>
     </>
