4fa14d3c5af53ae7682f8812018bb1c8ef357406
Muskulee <muskulee@gmail.com>
2024-02-24 16:18:35 +0100

    Added Payment Methods Array

--- app/(admin)/setting/PaymentSettings.tsx
+++ app/(admin)/setting/PaymentSettings.tsx
@@ -1,8 +1,51 @@
+import { Accordion, AccordionItem, Avatar, Switch } from "@nextui-org/react";
+import { BiAnchor, BiDownArrow, BiUpArrow, BiUpvote } from "react-icons/bi";
+import { BsMoonFill, BsPaypal } from "react-icons/bs";
+import { FaStripe } from "react-icons/fa6";
+import { FiSun } from "react-icons/fi";
+import { PiStripeLogo } from "react-icons/pi";
+
 const PaymentSettings = () => {
+  const PaymentMethods = [
+    {
+      title: "Paypal",
+      icon: <BsPaypal size={50} />,
+      component: <></>,
+    },
+    {
+      title: "Stripe",
+      icon: <PiStripeLogo size={50} />,
+      component: <></>,
+    },
+  ];
+
   return (
-    <>
-      <div>Payment Setting</div>
-    </>
+    <Accordion selectionMode="multiple">
+      {PaymentMethods.map((method, index) => {
+        return (
+          <AccordionItem
+            indicator={({ isOpen }) =>
+              isOpen ? <BiUpArrow /> : <BiDownArrow />
+            }
+            key={index}
+            aria-label={method.title}
+            startContent={<Avatar isBordered radius="sm" icon={method.icon} />}
+            subtitle={
+              <p className="flex">
+                <Switch
+                  defaultSelected
+                  aria-label={method.title}
+                  color="danger"
+                />
+              </p>
+            }
+            title={method.title}
+          >
+            {method.component}
+          </AccordionItem>
+        );
+      })}
+    </Accordion>
   );
 };
 
